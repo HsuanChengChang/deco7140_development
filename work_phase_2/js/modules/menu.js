ba561aa4eccce84afd8f4ba31108687f4eb9807a
HsuanChengChang <hsuancheng.chang@student.uq.edu.au>
2025-09-09 00:48:22 +1000

    Add Accessibility & Ethics assessment
    Add Accessibility & Ethics assessment

---
+++ work_phase_2/js/modules/menu.js
@@ -0,0 +1,8 @@
+export function setupMenu() {
+    const toggleBtn = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    toggleBtn.addEventListener("click", () => {
+        menu.classList.toggle("show");
+    });
+}
